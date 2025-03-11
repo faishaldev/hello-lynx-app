8385bb87101f5773369cd01baa3daf13bce5f75b
Faishal <musthafafaishal@gmail.com>
2025-03-11 23:49:31 +0700

    chore: update the environment for web

--- lynx.config.ts
+++ lynx.config.ts
@@ -16,4 +16,12 @@ export default defineConfig({
     }),
     pluginReactLynx(),
   ],
+  environments: {
+    web: {
+      output: {
+        assetPrefix: '/',
+      },
+    },
+    lynx: {},
+  },
 });
